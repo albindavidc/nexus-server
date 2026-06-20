bc626d640c3c5f75891f97cadb9a2e37c639d919
Albin David C <albindavidc007@gmail.com>
2026-06-20 15:33:19 +0530

    feat(notificaiton): create push notification service

---
+++ shared/utils/push.util.ts
@@ -0,0 +1,40 @@
+import webpush from "web-push";
+import { IPushSubscription } from "../../modules/auth/push-subscription.model";
+import logger from "./logger";
+
+webpush.setVapidDetails(
+  process.env.VAPID_SUBJECT!,
+  process.env.VAPID_PUBLIC_KEY!,
+  process.env.VAPID_PRIVATE_KEY!,
+);
+
+export interface IPushNotification {
+  title: string;
+  body: string;
+  icon?: string;
+  url?: string;
+}
+
+export const sendPushNotification = async (
+  subscription: IPushSubscription,
+  payload: IPushNotification,
+) => {
+  try {
+    await webpush.sendNotification(subscription, JSON.stringify(payload));
+  } catch (error) {
+    if (typeof error === "object" && error !== null && "statusCode" in error) {
+      const statusCode = (error as { statusCode: number }).statusCode;
+
+      if (statusCode === 404 || statusCode === 410) {
+        logger.warn(
+          `Push Subscription ${subscription.endpoint} not found or expired, deleting`,
+        );
+        await subscription.deleteOne();
+        return false;
+      }
+    }
+    logger.error(`Error sending push notification`, error);
+
+    throw error;
+  }
+};
